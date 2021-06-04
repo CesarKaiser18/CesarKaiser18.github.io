18840afee1e88c648f61b2676d01868fbfb0b0e9
CesarKaiser18 <cesar.alejandro.castro2000@gmail.com>
2021-06-03 22:24:39 -0500

    todo

--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 
 firebase.initializeApp({
   apiKey: "AIzaSyD_0J8R6cUg5DOxnbp7lZxmwp45CbcJcuA",
-    authDomain: "bdfalu20.firebaseapp.com",
-    projectId: "bdfalu20",
-    storageBucket: "bdfalu20.appspot.com",
-    messagingSenderId: "322493166158",
-    appId: "1:322493166158:web:4fc29ec04c7fe497ee891f",
-    measurementId: "G-LTSM7LY5RJ"
+  authDomain: "bdfalu20.firebaseapp.com",
+  projectId: "bdfalu20",
+  storageBucket: "bdfalu20.appspot.com",
+  messagingSenderId: "322493166158",
+  appId: "1:322493166158:web:4fc29ec04c7fe497ee891f",
+  measurementId: "G-LTSM7LY5RJ"
     
 });
 
